59791a2451e581601b3573a223a84c14c77cc5ac
juanpablou22 <juanpabloum25@gmail.com>
2025-05-16 19:37:37 -0500

    Creaccion de papuete MVC

---
+++ Taller_Practico_Interfaces/src/main/java/com/mycompany/taller_practico_interfaces/Taller_Practico_Interfaces.java
@@ -0,0 +1,11 @@
+
+
+package com.mycompany.taller_practico_interfaces;
+
+
+public class Taller_Practico_Interfaces {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

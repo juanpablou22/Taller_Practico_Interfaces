146fc2f9fa808b90db238d11307bfb66dd3f91e8
juanpablou22 <juanpabloum25@gmail.com>
2025-05-16 19:41:46 -0500

    Creaccion de proyecto

---
+++ TallerPInterfaces/src/main/java/com/mycompany/tallerpinterfaces/TallerPInterfaces.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tallerpinterfaces;
+
+/**
+ *
+ * @author Juan
+ */
+public class TallerPInterfaces {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
